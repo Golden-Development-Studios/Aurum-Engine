4782236b9cfa193c20800b23b1eb68fb919d6d9e
Golden Dev <sjpengelly15@gmail.com>
2025-10-27 18:52:29 +1300

    added content to main
    had to add content to main in order to get a successful first build of CMake

---
+++ src/Sandbox/src/main.cpp
@@ -0,0 +1,16 @@
+// main.cpp
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    std::cout << "Aurum Sandbox starting up..." << std::endl;
+
+    // Initialize your engine
+    // Example:
+    // Aurum::Engine engine;
+    // engine.Initialize();
+    // engine.Run();
+
+    return 0;
+}
