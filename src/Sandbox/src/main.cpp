67bba0ced1127fa57bdf6da0a65242f4e26ffed1
Golden Dev <sjpengelly15@gmail.com>
2025-11-01 11:16:36 +1300

    Updated Sandbox main.cpp
    successful test run with this main.cpp

--- src/Sandbox/src/main.cpp
+++ src/Sandbox/src/main.cpp
@@ -1,35 +1,26 @@
 #include <Framework/Logger.hpp>
-#include <Framework/Config.hpp>
+#include <Framework/MemoryTracker.hpp>
 #include <Framework/Timer.hpp>
-#include <thread> // for std::this_thread::sleep_for
+#include <thread>
 
 int main()
 {
     using namespace Aurum;
 
     Logger::Get().SetLogFile("AurumLog.txt");
-    Logger::Get().Log("=== Aurum Sandbox with Timer Test ===", LogLevel::Info);
+    Logger::Get().Log("=== Aurum Memory Tracker Test ===", LogLevel::Info);
 
-    // --- HighResolutionTimer example ---
-    HighResolutionTimer timer;
-    std::this_thread::sleep_for(std::chrono::milliseconds(150));
-    Logger::Get().Log("Manual Timer: " + std::to_string(timer.ElapsedMilliseconds()) + " ms");
-
-    // --- ScopedTimer example ---
     {
-        ScopedTimer scoped("Scoped Workload");
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
-    } // automatically logs upon scope exit
-
-    // --- FrameTimer example ---
-    FrameTimer frameTimer;
-    for (int i = 0; i < 3; ++i)
-    {
-        std::this_thread::sleep_for(std::chrono::milliseconds(16)); // simulate 60 FPS
-        double delta = frameTimer.Tick();
-        Logger::Get().Log("Frame " + std::to_string(i) + " delta: " + std::to_string(delta * 1000.0) + " ms");
+        ScopedTimer scoped("Allocation Block");
+        int* a = AURUM_NEW(int, 42);
+        double* b = AURUM_NEW(double, 3.14159);
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        AURUM_DELETE(a);
+        AURUM_DELETE(b);
     }
 
-    Logger::Get().Log("=== Timer Test Complete ===", LogLevel::Info);
+    MemoryTracker::Get().Report();
+
+    Logger::Get().Log("=== Memory Tracker Test Complete ===", LogLevel::Info);
     return 0;
 }
