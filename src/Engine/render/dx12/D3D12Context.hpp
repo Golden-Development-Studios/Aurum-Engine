5c83627bd732a57e25712874e8771e87ecd65fbc
Golden Dev <sjpengelly15@gmail.com>
2025-11-07 20:57:37 +1300

    updated DRD12Context.hpp
    Wraped duplicate definitions

--- src/Engine/render/dx12/D3D12Context.hpp
+++ src/Engine/render/dx12/D3D12Context.hpp
@@ -7,7 +7,9 @@
 #define NOMINMAX
 #endif
 
-#define WIN32_LEAN_AND_MEAN
+#ifndef WIN32_LEAN_AND_MEAN
+    #define WIN32_LEAN_AND_MEAN
+#endif
 #include <Windows.h>
 #include <d3d12.h>
 #include <dxgi1_6.h>
